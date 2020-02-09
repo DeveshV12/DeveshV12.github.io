a6c203bf58258adf69be44d91948cf1fc97a1840
DeveshV <57789247+DeveshV12@users.noreply.github.com>
2020-02-09 18:11:07 +0530

    Update views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request=request,template_name='html/mainapp/home.html')
+	return render(request=request,template_name='index.html')
